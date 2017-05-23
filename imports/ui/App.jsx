b125ef082232deb6fcefb5d05c70c235abfffc29
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2017-05-22 23:38:28 -0500

    login-signup frontend

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,11 +6,25 @@ import styles from './css/magnific-popup.css';
 import styles2 from './css/bootstrap.min.css';
 import styles3 from './css/magnific-popup.css';
 import styles4 from './css/font-awesome.min.css';
+import styles5 from './css/style.css';
+import styles6 from './css/slippry.css';
+import styles7 from './css/megamenu.css';
+import styles8 from './css/jquery-ui.css';
+import styles9 from './css/commerce.css';
+import styles10 from './css/elegant-icon.css';
+import styles11 from './css/jquery.selectBox.css';
+import styles12 from './css/prettyPhoto.css';
+import styles13 from './css/settings.css';
+import styles14 from './css/style-selector.css';
+import styles15 from './css/swatches-and-photos.css';
+import styles16 from './css/jstarbox.css';
 import jquery from './js/jquery.js';
 import script1 from './js/script.js';
 import bootstrap from './js/bootstrap.min.js';
+import jsStarbox from './js/jstarbox.js';
 import Modal from './Modal.jsx'
 
+const BASE_URL = "a";
 
 export class App extends Component {
   constructor(props) {
@@ -23,13 +37,131 @@ export class App extends Component {
 
   render() {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-4">
-            <a data-rel="loginModal" href="#"> Login </a>
-            <Modal/>
+      <div>
+        <div className="header">
+          <div className="top-header">
+  					<div className="wrap">
+  						<div className="top-header-right">
+  							<ul>
+  								<li><a data-rel="loginModal" href="#">Ingresar</a><span> </span></li>
+  								<li><a data-rel="registerModal" href="#">Registrate</a></li>
+  							</ul>
+  						</div>
+  						<div className="clear"> </div>
+  					</div>
+  				</div>
+          <div className="mid-header">
+            <div className="wrap">
+              <div className="mid-grid-left">
+                <form>
+                  <input type="text" placeholder="What Are You Looking for?"/>
+                </form>
+              </div>
+              <div className="mid-grid-right">
+                <a className="logo" href="index.html"><span> </span></a>
+              </div>
+              <div className="clear"> </div>
+            </div>
           </div>
         </div>
+        <div className="content">
+          <div className="wrap">
+            <div className="content-left">
+						<div className="content-left-top-grid">
+							<div className="content-left-price-selection">
+								<h4>Select Price:</h4>
+								<div className="price-selection-tree">
+									<span className="col_checkbox">
+										<input id="10" className="css-checkbox10" type="checkbox"/>
+										<label className="normal"><i htmlFor="10" name="demo_lbl_10" className="css-label10"> </i> 400</label>
+									</span>
+									<span className="col_checkbox">
+										<input id="11" className="css-checkbox11" type="checkbox"/>
+										<label className="active1"><i htmlFor="11" name="demo_lbl_11" className="css-label11"> </i>350</label>
+									</span>
+									<span className="col_checkbox">
+										<input id="12" className="css-checkbox12" type="checkbox"/>
+										<label className="normal"><i htmlFor="12" name="demo_lbl_12" className="css-label12"> </i> 300</label>
+									</span>
+									<span className="col_checkbox">
+										<input id="13" className="css-checkbox13" type="checkbox"/>
+										<label className="normal"><i htmlFor="13" name="demo_lbl_13" className="css-label13"> </i>250</label>
+									</span>
+									<span className="col_checkbox">
+										<input id="14" className="css-checkbox14" type="checkbox"/>
+										<label className="normal"><i htmlFor="14" name="demo_lbl_14" className="css-label14"> </i> 200</label>
+									</span>
+									<span className="col_checkbox">
+										<input id="15" className="css-checkbox15" type="checkbox"/>
+										<label className="normal"><i htmlFor="15" name="demo_lbl_15" className="css-label15"> </i>150</label>
+									</span>
+								</div>
+
+						</div>
+						</div>
+						<div className="content-left-bottom-grid">
+							<h4>Boys Football:</h4>
+							<div className="content-left-bottom-grids">
+								<div className="content-left-bottom-grid1">
+									<img src="images/foot-ball.jpg" title="football"/>
+									<h5><a href="details.html">Nike Strike PL Hi-Vis</a></h5>
+									<span> Football</span>
+									<label>£ 375</label>
+								</div>
+								<div className="content-left-bottom-grid1">
+									<img src="images/jarse.jpg" title="jarse"/>
+									<h5><a href="details.html">Nike Strike PL Hi-Vis</a></h5>
+									<span> Football</span>
+									<label>£ 375</label>
+								</div>
+							</div>
+						</div>
+				</div>
+				<div className="content-right">
+					<div className="product-grids">
+						<div className="product-grid">
+							<div className="product-grid-head">
+								<ul className="grid-social">
+									<li><a className="facebook" href="#"><span> </span></a></li>
+									<li><a className="twitter" href="#"><span> </span></a></li>
+									<li><a className="googlep" href="#"><span> </span></a></li>
+									<div className="clear"> </div>
+								</ul>
+								<div className="block">
+									<div className="starbox small ghosting"><div className="positioner"><div className="stars"><div className="ghost"></div><div className="colorbar"></div><div className="star_holder"><div className="star star-0"></div><div className="star star-1"></div><div className="star star-2"></div><div className="star star-3"></div><div className="star star-4"></div></div></div></div></div> <span> (46)</span>
+								</div>
+							</div>
+							<div className="product-pic">
+								<a href="#"><img src="images/product2.jpg" title="product-name"/></a>
+								<p>
+								<a href="#"><small>Nike</small> HYPERVENOM <small>Phantom</small> FG</a>
+								<span>Men's Firm-Ground Football Boot</span>
+								</p>
+							</div>
+							<div className="product-info">
+								<div className="product-info-cust">
+									<a href="details.html">Details</a>
+								</div>
+								<div className="product-info-price">
+									<a href="details.html">£ 380</a>
+								</div>
+								<div className="clear"> </div>
+							</div>
+							<div className="more-product-info">
+								<span> </span>
+							</div>
+						</div>
+						<div className="clear"> </div>
+					</div>
+				</div>
+				<div className="clear"> </div>
+			</div>
+		</div>
+        <div className="row">
+          <div className="col-md-4">
+          </div>
+        </div>
+        <Modal url={BASE_URL}/>
       </div>
     );
   }
--- imports/ui/Modal.jsx
+++ imports/ui/Modal.jsx
@@ -3,27 +3,79 @@ import { Meteor } from "meteor/meteor";
 
 export default class Modal extends Component {
 
+  constructor(props) {
+    super(props);
+  }
+
+  login(event) {
+    event.preventDefault();
+    var a = this;
+    jQuery(document).ready(function($) {
+      var loginInfo = {};
+      $.each($('#userloginModalForm').serializeArray(), function(i, field) {
+        loginInfo[field.name] = field.value;
+      });
+      jQuery.ajax({
+            url: a.props.url + "/login/",
+            type: "POST",
+            data: JSON.stringify(loginInfo),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function(data) {
+              //TODO
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+            }
+        });
+    });
+  }
+
+  signUp(event) {
+    event.preventDefault();
+    var a = this;
+    jQuery(document).ready(function($) {
+      var signUpInfo = {};
+      $.each($('#userregisterModalForm').serializeArray(), function(i, field) {
+        signUpInfo[field.name] = field.value;
+      });
+      jQuery.ajax({
+            url: a.props.url + "/signUp/",
+            type: "POST",
+            data: JSON.stringify(signUpInfo),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function(data) {
+              //TODO
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+            }
+        });
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="modal fade user-login-modal" id="userloginModal" tabIndex="-1" role="dialog" aria-hidden="true">
           <div className="modal-dialog">
             <div className="modal-content">
-              <form id="userloginModalForm" action="javascript:login()">
+              <form id="userloginModalForm" onSubmit={this.login.bind(this)}>
                 <div className="modal-header">
                   <button type="button" className="close" data-dismiss="modal">
-                    <span aria-hidden="true">&times;</span><span className="sr-only">Close</span>
+                    <span aria-hidden="true">&times;</span><span className="sr-only">Cerrar</span>
                   </button>
-                  <h4 className="modal-title">Login</h4>
+                  <h4 className="modal-title">Ingresar</h4>
                 </div>
                 <div className="modal-body">
                   <div className="form-group">
-                    <label htmlFor="user_name_enterprise">Nombre Empresa</label>
-                    <input type="text" id="user_nombre_empresa" required value="" name="nombreEmpresa" className="form-control" placeholder="Nombre Empresa" />
+                    <label htmlFor="user_name_enterprise">Usuario</label>
+                    <input type="text" id="usuario" required name="usuario" className="form-control" placeholder="Usuario" />
                   </div>
                   <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" required value="" name="password" className="form-control" placeholder="Password"/>
+                    <label htmlFor="password">Contraseña</label>
+                    <input type="password" id="password" required name="password" className="form-control" placeholder="Contraseña"/>
                   </div>
                 </div>
                 <div className="modal-footer">
@@ -39,7 +91,7 @@ export default class Modal extends Component {
         <div className="modal fade user-register-modal" id="userregisterModal" tabIndex="-1" role="dialog" aria-hidden="true">
           <div className="modal-dialog">
             <div className="modal-content">
-              <form id="userregisterModalForm" action="javascript:singUp()">
+              <form id="userregisterModalForm" onSubmit={this.signUp.bind(this)}>
                 <div className="modal-header">
                   <button type="button" className="close" data-dismiss="modal">
                     <span aria-hidden="true">&times;</span><span className="sr-only">Close</span>
@@ -49,27 +101,19 @@ export default class Modal extends Component {
                 <div className="modal-body">
                   <div className="form-group">
                     <label htmlFor="userName">Nombre</label>
-                    <input type="text" id="user_first_name" name="first_name" required className="form-control" value="" placeholder="Nombres"/>
+                    <input type="text" id="user_first_name" name="first_name" required className="form-control" placeholder="Nombres"/>
                   </div>
                   <div className="form-group">
                     <label htmlFor="userLastName">Apellidos</label>
-                    <input type="text" id="user_last_name" name="last_name" required className="form-control" value="" placeholder="Apellidos"/>
+                    <input type="text" id="user_last_name" name="last_name" required className="form-control" placeholder="Apellidos"/>
                   </div>
                   <div className="form-group">
                     <label htmlFor="user_email">Email</label>
-                    <input type="email" id="user_email" name="email" required className="form-control" value="" placeholder="Email"/>
+                    <input type="email" id="user_email" name="email" required className="form-control" placeholder="Email"/>
                   </div>
                   <div className="form-group">
                     <label htmlFor="user_password">Contraseña</label>
-                    <input type="password" id="user_password" required value="" name="password" className="form-control" placeholder="Mínimo 5 caracteres"/>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="user_password">Confirmación Contraseña</label>
-                    <input type="password" id="user_password2" required value="" name="password2" className="form-control" placeholder="Mínimo 5 caracteres"/>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="user_name_enterprise">Nombre Empresa</label>
-                    <input type="text" id="user_nombre_empresa" required value="" name="nombreEmpresa" className="form-control" placeholder="Mínimo 5 caracteres" />
+                    <input type="password" id="user_password" required name="password" className="form-control" placeholder="Contraseña"/>
                   </div>
                 </div>
                 <div className="modal-footer">
